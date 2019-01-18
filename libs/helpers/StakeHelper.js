f0ac02586adc486993ca19fb4173e3f16a7d430e
abhayks1 <abhay@ost.com>
2019-01-18 20:05:02 +0530

    Added private methods _getStakeRequestHashForStaker, _getStakeRequest

--- libs/helpers/StakeHelper.js
+++ libs/helpers/StakeHelper.js
@@ -92,6 +92,90 @@ class StakeHelper {
   }
 
   /**
+   * Facilitator performs accept stake request.
+   *
+   * @param facilitator - Facilitator address.
+   * @param stakeRequestHash - Stake request hash.
+   * @param r - R of the signature.
+   * @param s - S of the signature.
+   * @param v - V of the signature.
+   * @param originWeb3 - Origin chain web3 object.
+   * @param txOptions - Transaction options.
+   * @returns {PromiseLike<T> | Promise<T>} - Promise object
+   */
+  acceptStakeRequest(facilitator, stakeRequestHash, r, s, v, originWeb3, txOptions){
+    const oThis = this;
+    const txObject = oThis._acceptStakeRequestRawTx(facilitator, stakeRequestHash, r, s, v, originWeb3, txOptions);
+
+    let txReceipt = null;
+    return txObject
+      .send(txOptions)
+      .on('transactionHash', function(transactionHash) {
+        console.log('\t - transaction hash:', transactionHash);
+      })
+      .on('error', function(error) {
+        console.log('\t !! Error !!', error, '\n\t !! ERROR !!\n');
+        return Promise.reject(error);
+      })
+      .on('receipt', function(receipt) {
+        txReceipt = receipt;
+        console.log('\t - Receipt:\n\x1b[2m', JSON.stringify(receipt), '\x1b[0m\n');
+      })
+      .then(function(instance) {
+        oThis.address = instance.options.address;
+        console.log(`\t - ${ContractName} Contract Address:`, oThis.address);
+        return txReceipt;
+      });
+  }
+
+  /**
+   * Facilitator performs accept stake request.
+   *
+   * @param facilitator - Facilitator address.
+   * @param stakeRequestHash - Stake request hash.
+   * @param r - R of the signature.
+   * @param s - S of the signature.
+   * @param v - V of the signature.
+   * @param originWeb3 - Origin chain web3 object.
+   * @param txOptions - Transaction options.
+   * @returns {PromiseLike<T> | Promise<T>} - Promise object
+   */
+  _acceptStakeRequestRawTx(facilitator, stakeRequestHash, r, s, v, originWeb3, txOptions){
+    const oThis = this;
+
+    const web3 = originWeb3 || oThis.originWeb3;
+    const abiBinProvider = oThis.abiBinProvider;
+    const abi = abiBinProvider.getABI(ContractName);
+
+    let defaultOptions = {
+      from: facilitator,
+      to: oThis.gatewayComposerAddress,
+      gas: '8000000'
+    };
+
+    if (txOptions) {
+      Object.assign(defaultOptions, txOptions);
+    }
+    txOptions = defaultOptions;
+
+    const Contract = new web3.eth.Contract(
+      abi,
+      oThis.gatewayComposerAddress,
+      txOptions
+    );
+
+    console.log("_acceptStakeRequestRawTx: Returning txObject.");
+    const txObject =  Contract.methods.acceptStakeRequest(
+      stakeRequestHash,
+      r,
+      s,
+      v
+    );
+
+    return txObject;
+  }
+
+  /**
    * Performs request stake on GatewayComposer.
    *
    * @param owner - Owner of GatewayComposer contract.
@@ -139,6 +223,7 @@ class StakeHelper {
 
     const contract = new web3.eth.Contract(abi, oThis.gatewayComposer, txOptions);
 
+    console.log("Constructed txObject for GatewayComposer.requestStake.");
     const txObject = contract.methods.requestStake(
       stakeVTAmountInWei,
       mintBTAmountInWei,
@@ -151,6 +236,45 @@ class StakeHelper {
 
     return txObject;
   }
+
+  /**
+   * Returns stakeRequestHash for staker address.
+   * @param staker - Staker address.
+   * @param originWeb3 - Origin chain web3 object.
+   * @param txOptions - Tx options.
+   * @returns {String} - Hash unique for each stake request.
+   * @private
+   */
+  _getStakeRequestHashForStaker(staker, originWeb3, txOptions){
+    const oThis = this;
+
+    const web3 = originWeb3 || oThis.web3;
+    const abi = abiBinProvider.getABI(gatewayComposerContractName);
+    const contract = new web3.eth.Contract(abi, oThis.gatewayComposer, txOptions);
+
+    console.log("Fetching stake request hash for staker");
+    return contract.methods.stakeRequestHashes(staker).call();
+  }
+
+  /**
+   * Returns StakeRequest for a given StakeRequestHash.
+   * @param stakeRequestHash - Hash unique for each stake request.
+   * @param originWeb3 - Origin chain web3 object.
+   * @param txOptions - Tx options.
+   * @returns {Object} - Includes information related with requested stake.
+   * @private
+   */
+  _getStakeRequest(stakeRequestHash, originWeb3, txOptions){
+    const oThis = this;
+
+    const web3 = originWeb3 || oThis.web3;
+    const abi = abiBinProvider.getABI(gatewayComposerContractName);
+    const contract = new web3.eth.Contract(abi, oThis.gatewayComposer, txOptions);
+
+    console.log("Fetching stake request hash for staker");
+    return contract.methods.stakeRequestHashes(stakeRequestHash).call();
+  }
+
 }
 
 module.exports = StakeHelper;
