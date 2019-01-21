d98b436e9cf064259045ee1e553a4fe8b59a97d2
Gulshan Vasnani <gulshan@ost.com>
2019-01-21 17:02:44 +0530

    Added requestStake;mocktoken abi and bin;mockcontractsdeployer and unit testcase to test requestStake

--- libs/helpers/StakeHelper.js
+++ libs/helpers/StakeHelper.js
@@ -15,9 +15,8 @@ class StakeHelper {
    * @param originWeb3 - Origin chain web3 address.
    * @param brandedTokenAddress - Branded Token contract address.
    * @param gatewayComposerAddress - Gateway composer contract address.
-   * @param txOptions - Tx options.
    */
-  constructor(originWeb3, brandedTokenAddress, gatewayComposerAddress, txOptions) {
+  constructor(originWeb3, brandedTokenAddress, gatewayComposerAddress) {
     const oThis = this;
     oThis.originWeb3 = originWeb3;
     oThis.gatewayComposer = gatewayComposerAddress;
@@ -30,7 +29,46 @@ class StakeHelper {
     // requestStake();
   }
 
-  // TODO method to get mintBTAmountInWei from BT.
+  /**
+   * Returns the amount of branded tokens equivalent to a given amount of value tokens.
+   *
+   * @param vtAmountInWei - ValueToken amount to convert.
+   * @param brandedTokenContractAddress - BrandedToken contract address.
+   * @param originWeb3 - Origin chain web3 address.
+   * @param txOptions - Tx options.
+   */
+  convertToBTToken(vtAmountInWei, brandedTokenContractAddress, originWeb3, txOptions) {
+    const oThis = this;
+
+    const web3 = originWeb3 || oThis.originWeb3;
+    const brandedToken = brandedTokenContractAddress || oThis.brandedToken;
+    const abi = oThis.abiBinProvider.getABI(brandedTokenContractName);
+    const contract = new web3.eth.Contract(abi, brandedToken, txOptions);
+
+    console.log('Getting Branded tokens equivalent to Value tokens');
+    return contract.methods.convertToBrandedTokens(vtAmountInWei).call();
+  }
+
+  /**
+   * Approve gateway composer for ValueToken.
+   *
+   * @param valueTokenContractAddress Value token contract address.
+   * @param valueTokenAbi Value token ABI.
+   * @param amountInWei Amount to approve.
+   * @param originWeb3 Origin chain web3.
+   * @param txOptions Tx options.
+   */
+  approveForValueToken(valueTokenContractAddress, valueTokenAbi, amountInWei, originWeb3, txOptions) {
+    const oThis = this;
+
+    const web3 = originWeb3 || oThis.originWeb3;
+
+    if (valueTokenAbi.length == 0) {
+      throw Error('Value token abi is not provided');
+    }
+    const contract = new web3.eth.Contract(valueTokenAbi, valueTokenContractAddress, txOptions);
+    return contract.methods.approve(oThis.gatewayComposer, amountInWei);
+  }
 
   /**
    * Performs request stake on GatewayComposer.
@@ -78,10 +116,10 @@ class StakeHelper {
 
     return txObject
       .send(txOptions)
-      .on('transactionHash', function(transactionHash) {
+      .on('transactionHash of request stake', function(transactionHash) {
         console.log('\t - transaction hash:', transactionHash);
       })
-      .on('receipt', function(receipt) {
+      .on('receipt of request stake', function(receipt) {
         txReceipt = receipt;
         console.log('\t - Receipt:\n\x1b[2m', JSON.stringify(txReceipt), '\x1b[0m\n');
       })
@@ -150,6 +188,45 @@ class StakeHelper {
     );
 
     return txObject;
+  }
+
+  /**
+   * Returns stakeRequestHash for staker address.
+   * @param staker Staker address.
+   * @param originWeb3 Origin chain web3 object.
+   * @param txOptions Tx options.
+   * @returns {String} Request hash for the staker.
+   * @private
+   */
+  _getStakeRequestHashForStakerRawTx(staker, originWeb3, txOptions) {
+    const oThis = this;
+
+    const web3 = originWeb3 || oThis.web3;
+
+    const abi = oThis.abiBinProvider.getABI(brandedTokenContractName);
+
+    const contract = new web3.eth.Contract(abi, oThis.brandedToken, txOptions);
+
+    return contract.methods.stakeRequestHashes(staker).call();
+  }
+
+  /**
+   * Returns stake requestObject corresponding to the stake hash.
+   * @param stakeRequestHash - Hash of the requests done by the staker.
+   * @param originWeb3 - Origin chain web3 object.
+   * @param txOptions - Tx options.
+   * @returns {Object} Struct containing stake information.
+   * @private
+   */
+  _getStakeRequestRawTx(stakeRequestHash, originWeb3, txOptions) {
+    const oThis = this;
+
+    const web3 = originWeb3 || oThis.web3;
+
+    const abi = oThis.abiBinProvider.getABI(brandedTokenContractName);
+    const contract = new web3.eth.Contract(abi, oThis.brandedToken, txOptions);
+
+    return contract.methods.stakeRequests(stakeRequestHash).call();
   }
 }
 
