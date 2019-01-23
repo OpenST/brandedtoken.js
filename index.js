2504e15dcdd7896a59f7f25be436c7161024a182
abhayks1 <abhay@ost.com>
2019-01-23 18:25:38 +0530

    Commit includes: StakeHelper exposure in index.js

--- index.js
+++ index.js
@@ -3,8 +3,13 @@
 const AbiBinProvider = require('./libs/AbiBinProvider');
 const EconomySetup = require('./libs/EconomySetup');
 const Contracts = require('./libs/Contracts');
+const StakeHelper = require('./libs/helpers/StakeHelper');
+
 module.exports = {
   AbiBinProvider: AbiBinProvider,
   EconomySetup: EconomySetup,
-  Contracts: Contracts
+  Contracts: Contracts,
+  Helpers: {
+    StakeHelper: StakeHelper
+  }
 };
