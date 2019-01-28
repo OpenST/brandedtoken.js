f6804a60b2ba9a21ecb66f0dee643f0ee04de681
abhayks1 <abhay@ost.com>
2019-01-28 21:52:56 +0530

    Moved libs files to lib directory

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const AbiBinProvider = require('./libs/AbiBinProvider');
-const EconomySetup = require('./libs/EconomySetup');
-const Contracts = require('./libs/Contracts');
-const StakeHelper = require('./libs/helpers/stake/gateway_composer/StakeHelper');
-const Staker = require('./libs/helpers/stake/gateway_composer/Staker');
-const Facilitator = require('./libs/helpers/stake/gateway_composer/Facilitator');
+const AbiBinProvider = require('./lib/AbiBinProvider');
+const EconomySetup = require('./lib/EconomySetup');
+const Contracts = require('./lib/Contracts');
+const StakeHelper = require('./lib/helpers/stake/gateway_composer/StakeHelper');
+const Staker = require('./lib/helpers/stake/gateway_composer/Staker');
+const Facilitator = require('./lib/helpers/stake/gateway_composer/Facilitator');
 
 module.exports = {
   AbiBinProvider: AbiBinProvider,
