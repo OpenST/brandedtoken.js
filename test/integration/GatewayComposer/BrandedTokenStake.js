8fe450a8781dd42a233598d88c6d71ef3bdabf8a
Martin Schenck <martin@ost.com>
2019-02-22 16:53:11 +0100

    Inject StakeHelper into Facilitator

--- test/integration/GatewayComposer/BrandedTokenStake.js
+++ test/integration/GatewayComposer/BrandedTokenStake.js
@@ -1,23 +1,3 @@
-// Copyright 2019 OpenST Ltd.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// ----------------------------------------------------------------------------
-//
-// http://www.simpletoken.org/
-//
-// ----------------------------------------------------------------------------
-
 'use strict';
 
 // Load external packages
@@ -234,13 +214,15 @@ describe('Performs BrandedToken staking through GatewayComposer', async () => {
     const gatewayContractInstance = new Mosaic.ContractInteract.EIP20Gateway(originWeb3, caGateway);
     const bountyAmountInWei = await gatewayContractInstance.getBounty();
 
+
+    const stakeHelper = new StakeHelper(originWeb3, btAddress, gatewayComposerAddress);
+    const abiBinProviderFacilitator = new AbiBinProvider();
     const facilitatorInstance = new Facilitator(
       originWeb3,
       caMockToken,
-      btAddress,
-      gatewayComposerAddress,
       facilitator,
-      new AbiBinProvider(),
+      stakeHelper,
+      abiBinProviderFacilitator,
     );
     await facilitatorInstance.acceptStakeRequest(
       stakeRequestHash,
