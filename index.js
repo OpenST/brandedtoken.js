0bd8f742d89ab9ddc0505e8b774d58bccba5795b
Sarvesh Jain <sarvesh@ost.com>
2019-03-06 12:17:36 +0530

    Branded Token.js now exposes mosaic.js contract interacts

--- index.js
+++ index.js
@@ -20,8 +20,9 @@
 
 'use strict';
 
-/* eslint object-shorthand: "off" */
+const Mosaic = require('@openstfoundation/mosaic.js');
 
+/* eslint object-shorthand: "off" */
 const AbiBinProvider = require('./lib/AbiBinProvider');
 const BrandedToken = require('./lib/ContractInteract/BrandedToken');
 const Contracts = require('./lib/Contracts');
@@ -32,6 +33,8 @@ const StakeHelper = require('./lib/helpers/stake/gateway_composer/StakeHelper');
 const Staker = require('./lib/Staker');
 const UtilityBrandedToken = require('./lib/ContractInteract/UtilityBrandedToken');
 
+const mosaicContractInteracts = Mosaic.ContractInteract;
+
 module.exports = {
   AbiBinProvider,
   EconomySetup,
@@ -40,6 +43,7 @@ module.exports = {
     BrandedToken,
     GatewayComposer,
     UtilityBrandedToken,
+    ...mosaicContractInteracts,
   },
   Helpers: {
     StakeHelper,
