31d20c9c829cbde2ef38a6d46a36006cc80e2696
Sarvesh Jain <sarvesh@ost.com>
2019-03-26 17:24:10 +0530

    Turning on and off log to file based on enviornment variable

--- logger/index.js
+++ logger/index.js
@@ -6,23 +6,27 @@ const {
   combine, timestamp, prettyPrint,
 } = format;
 
-const index = createLogger({
+const logPath = process.env.BRANDED_TOKEN_JS_LOGGER_PATH || 'log';
+let defaultTransport = [];
+
+if (process.env.ENABLE_BRANDED_TOKEN_JS_LOGGER) {
+  defaultTransport = [
+    new transports.File({ filename: `${logPath}/error.log`, level: 'error' }),
+    new transports.File({ filename: `${logPath}/combined.log` }),
+  ];
+} else {
+  defaultTransport.push(new transports.Console({
+    format: format.simple(),
+  }));
+}
+const logger = createLogger({
   level: 'info',
   format: combine(
     timestamp(),
     prettyPrint(),
   ),
   defaultMeta: { service: 'JLP' },
-  transports: [
-    new transports.File({ filename: 'log/error.log', level: 'error' }),
-    new transports.File({ filename: 'log/combined.log' }),
-  ],
+  transports: defaultTransport,
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  index.add(new transports.Console({
-    format: format.simple(),
-  }));
-}
-
-module.exports = index;
+module.exports = logger;
