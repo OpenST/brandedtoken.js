ce5d3945f742fe51478d37218b7c304f53a752a2
Sarvesh Jain <sarvesh@ost.com>
2019-03-26 19:25:09 +0530

    Added logger in more classes

--- logger/index.js
+++ logger/index.js
@@ -3,29 +3,33 @@
 const { createLogger, format, transports } = require('winston');
 
 const {
-  combine, timestamp, prettyPrint,
+  combine, prettyPrint,
 } = format;
 
 const logPath = process.env.BRANDED_TOKEN_JS_LOGGER_PATH || 'log';
-let defaultTransport = [];
+let defaultTransport = [new transports.Console({
+  format: format.combine(
+    format.colorize(),
+    format.simple(),
+  ),
+})];
 
-if (process.env.ENABLE_BRANDED_TOKEN_JS_LOGGER) {
+const enableLogger = process.env.ENABLE_BRANDED_TOKEN_JS_LOGGER;
+if (enableLogger && enableLogger === true) {
   defaultTransport = [
     new transports.File({ filename: `${logPath}/error.log`, level: 'error' }),
     new transports.File({ filename: `${logPath}/combined.log` }),
   ];
-} else {
-  defaultTransport.push(new transports.Console({
-    format: format.simple(),
-  }));
 }
 const logger = createLogger({
-  level: 'info',
   format: combine(
-    timestamp(),
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
     prettyPrint(),
+
   ),
-  defaultMeta: { service: 'JLP' },
+  defaultMeta: { service: 'brandedtoken.js' },
   transports: defaultTransport,
 });
 
