845cfa74525e63d09cd15bd6bf6c0f6b7db29fe7
Sarvesh Jain <sarvesh@ost.com>
2019-03-27 13:51:20 +0530

    Added log levels support

--- logger/index.js
+++ logger/index.js
@@ -2,32 +2,39 @@
 
 const { createLogger, format, transports } = require('winston');
 
-const {
-  combine, prettyPrint,
-} = format;
+const { combine, prettyPrint } = format;
 
 const logPath = process.env.BRANDED_TOKEN_JS_LOGGER_PATH || 'log';
-let defaultTransport = [new transports.Console({
-  format: format.combine(
-    format.colorize(),
-    format.simple(),
-  ),
-})];
+const logLevel = process.env.LOG_LEVEL || 'info';
+const enableFileLogger = process.env.ENABLE_BRANDED_TOKEN_JS_LOGGER;
 
-const enableLogger = process.env.ENABLE_BRANDED_TOKEN_JS_LOGGER;
-if (enableLogger && enableLogger === true) {
+let defaultTransport = [
+  new transports.Console({
+    format: format.combine(
+      format.colorize(),
+      format.simple(),
+    ),
+    level: logLevel,
+  }),
+];
+
+if (enableFileLogger && enableFileLogger === 'true') {
   defaultTransport = [
-    new transports.File({ filename: `${logPath}/error.log`, level: 'error' }),
-    new transports.File({ filename: `${logPath}/combined.log` }),
+    new transports.File(
+      {
+        filename: `${logPath}/brandedTokenJS.log`,
+        level: logLevel,
+      },
+    ),
   ];
 }
+
 const logger = createLogger({
   format: combine(
     format.timestamp({
       format: 'YYYY-MM-DD HH:mm:ss',
     }),
     prettyPrint(),
-
   ),
   defaultMeta: { service: 'brandedtoken.js' },
   transports: defaultTransport,
